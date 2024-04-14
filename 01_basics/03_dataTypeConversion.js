4c22e1e2b356dd5d80f7587ca4e7c47a3c83c817
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-14 17:33:18 +0000

    coversion of data types

---
+++ 01_basics/03_dataTypeConversion.js
@@ -0,0 +1,29 @@
+let num=33
+console.log(typeof num)
+let stringNum= String(num);
+console.log(typeof stringNum)
+
+let isLogged=1;
+console.log(typeof isLogged)
+let booleanIslogged= Boolean(isLogged);
+console.log(booleanIslogged);
+/*
+1-> true
+0-> false
+" "-> false
+"diksha"-> true
+*/
+
+let value=3
+let negativeVal= -value;
+console.log(negativeVal);
+
+let str1="hello "
+let str2="diksha"
+let str3= str1 + str2;
+console.log(str3);
+
+console.log("1" + 2) // 12
+console.log(1 + "2") //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
