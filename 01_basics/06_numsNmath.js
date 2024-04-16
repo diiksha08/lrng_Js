3533ce7e22c3d0ae5dde40745e0732512bb168b6
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-16 15:06:17 +0000

    Numbers and Math

---
+++ 01_basics/06_numsNmath.js
@@ -0,0 +1,33 @@
+const score=500
+console.log(score);
+//or
+const balance= new Number(600)
+console.log(balance)
+console.log(balance.toString());
+console.log(typeof balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(balance.toFixed(1));
+
+const otherNum= 234.8973
+console.log(otherNum.toPrecision(3));
+console.log(otherNum.toPrecision(4));
+
+const hundreds=10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+//----------------------------------------------------------------------------------------------
+
+//MATHS
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(7.74));
+console.log(Math.ceil(7.74));
+console.log(Math.floor(7.74));
+console.log(Math.sqrt(81));
+console.log(Math.min(4,8,9,2));
+console.log(Math.max(4,8,9,2));
+
+console.log(Math.random());
+
+
