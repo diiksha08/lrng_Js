bd2b439cceeaf62cff5c2cd58c9e664e33934cfe
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-17 12:09:38 +0000

    dates and time in js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,17 @@
+//let myDate= new Date();
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate=new Date(2024, 3, 17)
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+//let myCreatedDate=new Date(2024, 3, 17,5,4)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate=new Date("2024-04-17")
+let myCreatedDate=new Date("04-17-2024")
+console.log(myCreatedDate.toLocaleString());
+
