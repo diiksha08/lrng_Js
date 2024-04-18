cfbc251a8f7e600f31e4657683b6e0568edcc249
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-18 16:18:53 +0000

    Array

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -15,3 +15,21 @@
 let myCreatedDate=new Date("04-17-2024")
 console.log(myCreatedDate.toLocaleString());
 
+//timestamp-----------------------------------------------------------------------------
+let myTimeStamp=Date.now()
+
+// console.log(myTimeStamp);
+
+// console.log(myCreatedDate.getTime());
+
+//convert ms into seconds
+console.log(Math.floor(Date.now()/1000));
+
+let newDate= new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default'){
+    weekday: 'long'
+    
+}
