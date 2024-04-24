eb6048e16192948bcc44ae28b906ea2356c65cb6
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-24 11:22:29 +0000

    functions

---
+++ 04_func/func1.js
@@ -0,0 +1,23 @@
+function sayMyname(){
+    // console.log("D");
+    // console.log("E");
+    // console.log("E");
+    // console.log("K");
+    // console.log("S");
+    // console.log("H");
+    // console.log("A");
+}
+
+//call a function
+sayMyname() //sayMyname <- reference, sayMyname() <- execution
+
+
+//function to add two numbers
+function addTwoNum(num1, num2){ //function ki definition banate hain, uske ander jo bhi input lete hain usko we call parameters
+    console.log(num1+num2)
+}
+
+// addTwoNum(3,"4") //function ko call karate time, jo value usme pass karte hain use we call arguments
+// addTwoNum(3, "a") //3a
+addTwoNum(3,null)  //3 
+
