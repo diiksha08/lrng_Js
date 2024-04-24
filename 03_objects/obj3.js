074eca1df6db691fec2b2123aad995e9f9e254c1
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-24 10:49:12 +0000

    json

---
+++ 03_objects/obj3.js
@@ -0,0 +1,30 @@
+//de-structure
+const course={
+    courseName:"js in hindi",
+    price:"999",
+    courseInstructor: "hitesh"
+
+}
+// course.courseInstructor
+const{courseInstructor}= course //extracting value from course object  //de-structuring
+console.log(courseInstructor);
+
+// can use alies: const{courseInstructor: instructor}=course
+//console.log(instructor)
+
+
+//APIs
+
+//json ->{}
+// {
+    // "name": "deeksha",
+    // "courseName": "js in hindi",
+    // "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
