7379b019bc07a72b4d5c0c67e491e2dd0620c4c0
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-25 06:45:36 +0000

    func1

--- 04_func/func1.js
+++ 04_func/func1.js
@@ -14,10 +14,37 @@ sayMyname() //sayMyname <- reference, sayMyname() <- execution
 
 //function to add two numbers
 function addTwoNum(num1, num2){ //function ki definition banate hain, uske ander jo bhi input lete hain usko we call parameters
-    console.log(num1+num2)
+    let result= num1+num2;
+   // console.log(result);
+    return result;
+    // console.log(num1+num2)
 }
 
 // addTwoNum(3,"4") //function ko call karate time, jo value usme pass karte hain use we call arguments
 // addTwoNum(3, "a") //3a
-addTwoNum(3,null)  //3 
+//addTwoNum(3,null)  //3 
+
+const result = addTwoNum(3,4)
+
+
+// function loginUser(username){
+
+    // return `${username} just logged in`
+// }
+
+// console.log(loginUser("deeksha"))
+// console.log(loginUser("")) // just logged in 
+
+
+function loginUser(username // or can pass a default value username= "simsim"){
+    if(username===undefined){  //in js using empty string->false, undefined->false considered in if else conditions  
+       //if(!username) or if(username===undefined)
+        console.log("please enter a username");
+        return
+    }
+
+    return `${username} just logged in`
+}
+
+console.log(loginUser()) // undefined 
 
