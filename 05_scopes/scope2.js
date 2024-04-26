e30caa63f6758fa49985aa1b3f9241221723b2c3
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-26 16:46:44 +0000

    scopes2

--- 05_scopes/scope2.js
+++ 05_scopes/scope2.js
@@ -5,8 +5,39 @@ function one(){
         const website="youtube"
         console.log(username);
     }
-    console.log(website);
+    // console.log(website); // website not defined; out of scope
     
     two()
+}
+// one()
+
+
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website=" youtube"
+        console.log(username+website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+// ++++++++++++++++++++++INTERESTING+++++++++++++++++++++
+
+console.log(addOne(5)) // can be accessed before and after initialization
+function addOne(num){  //function
+    return num+1 
+}
+
+/* 
+const addTwo= function(num){ // also a function; it's called expression
+    return num +2
+}
+console.log(addTwo(7)) 
+*/
+
+//console.log(addTwo(7)) //cannot access addTwo before initialization
+const addTwo= function(num){ // also a function; it's called expression
+    return num +2
 }
 
