5687c0012503fdba2065645ad5d2651c754947c6
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-04-26 16:08:27 +0000

    scopes2

---
+++ 05_scopes/scope2.js
@@ -0,0 +1,12 @@
+function one(){
+    const username="deeksha"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    console.log(website);
+    
+    two()
+}
+
