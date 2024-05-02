87af5ef440702968a759682b306158fa609f714e
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-05-02 09:59:08 +0000

    switch-case

---
+++ 08_controlFlow/switch.js
@@ -0,0 +1,52 @@
+// switch-case syntax:
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month= 3
+
+// switch (month) {
+//     case 1:         // checks value equal to 1 
+//         console.log("January");
+//         break;
+//     case 2:         // checks value equal to 2 
+//         console.log("Feb");
+//         break;
+//     case 3:         // checks value equal to 3 
+//         console.log("Mar");
+//         break;
+//     case 4:         // checks value equal to 4
+//         console.log("Apr");
+//         break;
+        
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const month= "March"
+
+switch (month) {
+    case "January":         // checks value equal to 1 
+        console.log("Jan");
+        break;
+    case "February":         // checks value equal to 2 
+        console.log("Feb");
+        break;
+    case "March":        // checks value equal to 3 
+        console.log("Mar");
+        break;
+    case "April":         // checks value equal to 4
+        console.log("Apr");
+        break;
+        
+    default:
+        console.log("default case match");
+        break;
+}
