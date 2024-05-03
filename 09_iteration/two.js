1f7a302c80f04f8184c31e1a3f385913df99b196
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-05-03 18:17:34 +0000

    while-do-while

---
+++ 09_iteration/two.js
@@ -0,0 +1,21 @@
+//while 
+
+let index=0;
+while(index<=10){
+    console.log(`value of index ${index}`);
+    index=index+2
+}
+
+let myArr=['flash', 'batman', 'spiderman']
+let i=0
+while(i<myArr.length){
+    console.log(`value is ${myArr[i]}`);
+    i=i+1;
+}
+
+//do-while
+let score=1;
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
