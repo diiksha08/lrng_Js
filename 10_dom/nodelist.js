363e466d905d3c51a3552fdb9be07d407abb936d
diiksha08 <161272353+diiksha08@users.noreply.github.com>
2024-05-10 09:05:20 +0000

    nodeList/HTMLCollection

---
+++ 10_dom/nodelist.js
@@ -0,0 +1,8 @@
+/*
+
+forEach can be used on nodeList but map() cannot, to use map() on nodeList:
+convert nodeList to Array by : Array.from("name of that nodeList")
+
+HTMLCollection
+to use forEach()/ map(), or any array method, need to convert HTMLCollection to
+Array by: Array.from('name of that HTMLCollection') 
